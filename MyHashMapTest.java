768e5dfc9ef5693b24c3f803a73eb56f72b07bcc
lynngiam <37986479+lynngiam@users.noreply.github.com>
2018-04-28 13:01:12 -0400

    Update MyHashMapTest.java

--- MyHashMapTest.java
+++ MyHashMapTest.java
@@ -1,17 +1,18 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class MyHashMapTest {
 
     @Test
     public void testPut() {
-	System.out.println("Test");
 	MyHashMap<String, Integer> test = new MyHashMap<String, Integer>(10, (float) 0.5);
 	HashMap<String, Integer> standard = new HashMap<String, Integer>(10, (float) 0.5);
 	test.put("A", 1);
@@ -83,18 +84,74 @@ public class MyHashMapTest {
 
     @Test
     public void testToString() {
-	fail("Not yet implemented");
+	MyHashMap<String, Integer> test = new MyHashMap<String, Integer>(10, (float) 0.5);
+	test.put("A", 1);
+	test.put("B", 2);
+	test.put("D", 4);
+	assertEquals("Convert to string: ", " Hash code: 5 | Keys: A | Value: 1\n"
+		+ " Hash code: 6 | Keys: B | Value: 2\n" + " Hash code: 8 | Keys: D | Value: 4\n" + "",
+		test.toString());
     }
 
+    @Ignore
     @Test
-    public void testGeneratePrime() {
+    public void testGeneratePrime() { // tested this method by converting method to public
 	MyHashMap<String, Integer> test = new MyHashMap<String, Integer>();
-	assertEquals("Next prime: ", 23, test.generatePrime(11));
+	// assertEquals("Next prime: ", 23, test.generatePrime(11));
+	// assertEquals("Next prime: ", 47, test.generatePrime(23));
     }
 
     @Test
     public void testResize() {
-	fail("Not yet implemented");
+	MyHashMap<String, Integer> test = new MyHashMap<String, Integer>(10, (float) 0.5);
+	HashMap<String, Integer> standard = new HashMap<String, Integer>(10, (float) 0.5);
+	test.put("A", 1);
+	test.put("B", 2);
+	test.put("C", 3);
+	test.put("D", 4);
+	test.put("E", 5);
+	test.put("F", 6);
+	test.put("G", 7);
+	standard.put("A", 1);
+	standard.put("B", 2);
+	standard.put("C", 3);
+	standard.put("D", 4);
+	standard.put("E", 5);
+	standard.put("F", 6);
+	standard.put("G", 7);
+	Iterator<String> itKeys = test.keys();
+	while (itKeys.hasNext()) {
+	    assertTrue(standard.containsKey(itKeys.next()));
+	}
+	assertEquals("Size: ", standard.size(), test.size());
+	assertEquals("Size: ", 7, test.size());
+    }
+
+    @Test
+    public void testIterator() {
+	MyHashMap<String, Integer> test = new MyHashMap<String, Integer>(10, (float) 0.5);
+	HashMap<String, Integer> standard = new HashMap<String, Integer>(10, (float) 0.5);
+	test.put("A", 1);
+	test.put("B", 2);
+	test.put("D", 4);
+	standard.put("A", 1);
+	standard.put("B", 2);
+	standard.put("D", 4);
+	// Test keys iterator
+	Set<String> standardKeys = standard.keySet();
+	Iterator<String> itStandardKeys = standardKeys.iterator();
+	Iterator<String> itTestKeys = test.keys();
+	while (itTestKeys.hasNext()) {
+	    assertEquals("Keys: ", itStandardKeys.next(), itTestKeys.next());
+	}
+	// Test entries iterator
+	Set<Map.Entry<String, Integer>> standardEntries = standard.entrySet();
+	Iterator<Map.Entry<String, Integer>> itStandardEntries = standardEntries.iterator();
+	Iterator<Map.Entry<String, Integer>> itTestEntries = test.entries();
+	while (itTestEntries.hasNext()) {
+	    assertEquals("Entries: ", itStandardEntries.next(), itTestEntries.next());
+	}
+
     }
 
 }

a31e7c5eb2d6b5d3ac97a06449ed5a60835b8579
lamchiminhmark <34977592+lamchiminhmark@users.noreply.github.com>
2018-04-29 18:12:47 -0400

    Update TextGenerator.java

--- TextGenerator.java
+++ TextGenerator.java
@@ -1,54 +1,60 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Random;
-import java.util.Scanner;
 
 public class TextGenerator {
 
     public static void main(String[] args) throws FileNotFoundException {
 	// TODO Auto-generated method stub
-	Scanner input = new Scanner(System.in);
-	System.out.println("Enter the order of the Markov Model (k): ");
-	int k = input.nextInt();
-	System.out.println("Enter the number of characters to generate (M): ");
-	int M = input.nextInt();
-	System.out.println("Enter the file name: ");
-	String fileName = input.next();
-	FileReader file = new FileReader(fileName);
-	MyHashMap<String, StringBuffer> mMap = FrequencyCounter.generateMap(k, fileName);
-	ArrayList<StringBuffer> output = new ArrayList<StringBuffer>();
-	StringBuffer starter = new StringBuffer();
-	for (int j = 0; j < k; j++) {
-	    try {
-		starter.append(file.read());
-	    } catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	// @@@@ added reading from the command line.
+	// @@@@ Changed variable String fileName to String file to comply with the lab
+	// instructions.
+	int k = Integer.parseInt(args[0]);
+	int M = Integer.parseInt(args[1]);
+	String file = args[2];
+
+	// @@@@ file becomes fileReader.
+	try {
+	    FileReader fileReader = new FileReader(file);
+
+	    MyHashMap<String, StringBuffer> mMap = FrequencyCounter.generateMap(k, file);
+	    StringBuffer starter = new StringBuffer();
+	    for (int j = 0; j < k; j++) {
+
+		// @@@@ added (char) to convert int to char.
+		starter.append((char) fileReader.read());
 	    }
-	}
-	String key = starter.toString();
-	while (mMap.get(key) != null) {
-	    int wordLength = k;
-	    while (wordLength < M) {
-		if (mMap.get(key) == null) {
-		    break;
+
+	    String key = starter.toString();
+	    // ???? I don't understand what output is for.
+	    // @@@@ changed this while loop to if block because the console is printing
+	    // floppyflopp floppyflopp for M = 10.
+	    if (mMap.get(key) != null) {
+		int wordLength = k;
+		while (wordLength < M) {
+		    if (mMap.get(key) == null) {
+			break;
+		    }
+		    StringBuffer sChars = mMap.get(key);
+		    Random rand = new Random();
+		    int randomIndex = rand.nextInt(sChars.length());
+		    starter.append(sChars.charAt(randomIndex));
+		    wordLength++;
+
+		    // @@@@ Changed this from (wordLength - k, wordLength) to (wordLength - k).
+		    key = starter.substring(wordLength - k);
 		}
-		StringBuffer sChars = mMap.get(key);
-		Random rand = new Random();
-		int randomIndex = rand.nextInt(sChars.length());
-		starter.append(sChars.charAt(randomIndex));
-		wordLength++;
-		key = starter.substring(wordLength - k, wordLength);
+		System.out.print(starter + " ");
+
 	    }
-	    output.add(starter);
+	    System.out.println();
+	} catch (FileNotFoundException e) {
+	    System.err.println(file + " is not found.");
+	    System.exit(2);
+	} catch (IOException e) {
+	    System.err.println("Error reading from file " + file + ": " + e.getMessage());
+	    System.exit(4);
 	}
-	for (StringBuffer word : output) {
-	    System.out.print(word + " ");
-	}
-	System.out.println(".");
-
     }
-
 }

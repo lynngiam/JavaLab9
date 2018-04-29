8af6ee18054856731b55d5da4bd3bdb29b9ae54d
lamchiminhmark <34977592+lamchiminhmark@users.noreply.github.com>
2018-04-28 22:02:41 -0400

    Add files via upload

--- FrequencyCounter.java
+++ FrequencyCounter.java
@@ -1,13 +1,14 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 
 public class FrequencyCounter {
 
 	public static void main(String[] args) {
-		Map<StringBuffer, StringBuffer> mMap = new MyHashMap<String, StringBuffer>();
+		Map<StringBuffer, StringBuffer> mMap = new MyHashMap<StringBuffer, StringBuffer>();
 		int k  = Integer.parseInt(args[0]);
 		System.out.print("Enter your file name: ");
 		Scanner input = new Scanner(System.in);
@@ -15,27 +16,41 @@ public class FrequencyCounter {
 		
 		
 		int nextChar;
-		char char1;
-		char char2;
-		char char3;
+		StringBuffer key = new StringBuffer();
 		try {
 			FileReader reader = new FileReader(inputFileName);
-			if ((nextChar = reader.read()) != -1) char1 = (char) nextChar;
-			if ((nextChar = reader.read()) != -1) char2 = (char) nextChar;
+			// TODO: add exception handling for k values larger than string length.
+			for (int i = 0; i < k; i++) {
+				key.append(reader.read());
+			}
 			
 			while ((nextChar = reader.read()) != -1) {
-				char3 = (char) nextChar;
-				StringBuffer key = new StringBuffer();
-				key.append(char1);
-				key.append(char2);
-				StringBuffer value = new StringBuffer();
-				value
+				char currentChar = (char) nextChar;
+				if (mMap.get(key) == null) {
+					StringBuffer value = new StringBuffer();
+					value.append(currentChar);
+					mMap.put(key, value);
+				} else {
+					mMap.get(key).append(currentChar);
+				}
+				
+				// Update key by removing the first char and adding new char.
+				key.deleteCharAt(0);
+				key.append(currentChar);
 			}
 		} catch (FileNotFoundException e){
 			System.err.println(inputFileName + " is not found.");
 		} catch (IOException e) {
 			System.err.println("Error reading from file " + inputFileName +
 					": " + e.getMessage());
+		}
+		
+		System.out.println(mMap.size() + "distinct keys:");
+		Iterator<Map.Entry<StringBuffer, StringBuffer>> it = mMap.entries();
+		while (it.hasNext()) {
+			Map.Entry<StringBuffer, StringBuffer> entry = it.next();
+			StringBuffer values = entry.getValue();
+			System.out.println(values.length() + " " + entry.getKey());			
 		}
 	}
 
